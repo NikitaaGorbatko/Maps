fa78bb8fcff06fd68d98b207fd34137229e9a004
Bastion Orpo <bastion.orpo@gmail.com>
2021-02-14 19:54:20 +0300

    step

--- app/src/main/java/nikitagorbatko/example/maps/MapsActivity.kt
+++ app/src/main/java/nikitagorbatko/example/maps/MapsActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
-import java.lang.NullPointerException
-import kotlin.math.PI
+import com.google.android.gms.maps.model.JointType
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlin.math.sin
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //longitude - Долгота x
@@ -20,6 +23,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var buttonAB: Button
     private var options: MarkerOptions? = null
+    private val sydneyLatLng = LatLng(-34.0, 151.0)
     private var latLngA: LatLng? = null
     private var latLngB: LatLng? = null
 
@@ -27,8 +31,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private var southwest: LatLng? = null
     private var northwest: LatLng? = null
     private var southeast: LatLng? = null
-
-    private var polylineABOptions: PolylineOptions? = null
+    private val DISTANCEM = 0.00018
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +43,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         buttonAB = findViewById<Button>(R.id.button).also {
             it.setOnClickListener {
-                polylineABOptions = null
                 mMap.clear()
                 latLngA = null
                 latLngB = null
@@ -60,6 +62,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         buttonAB.isEnabled = true
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydneyLatLng))
 
         mMap.setOnMapClickListener {
             if (latLngA != null && latLngB != null) return@setOnMapClickListener
@@ -77,15 +80,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
             val latLngC = Computer.getLatLangC(latLngA, latLngB)
 
-            polylineABOptions = PolylineOptions()
-                .add(latLngA, latLngB)
-                .width(3f)
-                .color(Color.BLUE)
-                .jointType(JointType.ROUND)
-
-            val polylineAB = mMap.addPolyline(
-                polylineABOptions
-            )
+            mMap.addPolyline(buildOptions(latLngA, latLngB))
 
             val a = Computer.getLength(latLngA, latLngC)
             val b = Computer.getLength(latLngB, latLngC)
@@ -93,98 +88,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val cornerA = Computer.getDegreesA(a, b)
             val cornerB = 180 - 90 - cornerA
 
-            val dotsRight = Computer.isCrossed(latLngA!!, latLngB!!, northeast!!, southeast!!)
-            val dotsLeft = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, southwest!!)
-            val dotsTop = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, northeast!!)
-            val dotsBottom = Computer.isCrossed(latLngA!!, latLngB!!, southeast!!, southwest!!)
-            var c: Double
+            draw()
 
-            if (Computer.getLength(dotsLeft, dotsRight) > Computer.getLength(dotsTop, dotsBottom)) {
-                val polylineAc = mMap.addPolyline(
-                    PolylineOptions()
-                        .add(dotsTop, dotsBottom)
-                        .width(3f)
-                        .color(Color.BLUE)
-                        .jointType(JointType.ROUND)
-                )
-                c = 0.00018 / sin(cornerA)//0.00018 - 20 meters in LatLng
-                var i = 100
-                var counter1 = dotsTop!!.longitude + c
-                var counter2 = dotsBottom!!.longitude + c
-                while (i > 0) {
-                    mMap.addPolyline(
-                        PolylineOptions()
-                            .add(LatLng(counter1, dotsTop.latitude), LatLng(counter2, dotsBottom.latitude))
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                    counter1 += c
-                    counter2 += c
-                    i--
-                }
-                counter1 = dotsTop!!.longitude - c
-                counter2 = dotsBottom!!.longitude - c
-                var k = 100
-                while (k > 0) {
-                    mMap.addPolyline(
-                        PolylineOptions()
-                            .add(LatLng(counter1, dotsTop.latitude), LatLng(counter2, dotsBottom.latitude))
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                    counter1 -= c
-                    counter2 -= c
-                    k--
-                }
-
-            } else {
-                val polylineAx = mMap.addPolyline(
-                    PolylineOptions()
-                        .add(dotsLeft, dotsRight)
-                        .width(3f)
-                        .color(Color.BLUE)
-                        .jointType(JointType.ROUND)
-                )
-                c = 0.00018 / sin(cornerB)//0.00018 - 20 meters in LatLng
-                var i = 100
-                var counter1 = dotsLeft!!.latitude + c
-                var counter2 = dotsRight!!.latitude + c
-                while (i > 0) {
-                    mMap.addPolyline(
-                        PolylineOptions()
-                            .add(LatLng(counter1, dotsLeft.longitude), LatLng(counter2, dotsRight.longitude))
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                    counter1 += c
-                    counter2 += c
-                    i--
-                }
-                counter1 = dotsLeft!!.latitude - c
-                counter2 = dotsRight!!.latitude - c
-                var k = 100
-                while (k > 0) {
-                    mMap.addPolyline(
-                        PolylineOptions()
-                            .add(LatLng(counter1, dotsLeft.longitude), LatLng(counter2, dotsRight.longitude))
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                    counter1 -= c
-                    counter2 -= c
-                    k--
-                }
-            }
-            Toast.makeText(baseContext, "$cornerA \n$cornerB\n$c", Toast.LENGTH_LONG).show()
             mMap.addMarker(options)
         }
 
         mMap.setOnCameraIdleListener {
             mMap.clear()
+
             northeast = mMap.projection.visibleRegion.latLngBounds.northeast
             southwest = mMap.projection.visibleRegion.latLngBounds.southwest
             northwest = LatLng(northeast!!.latitude, southwest!!.longitude)
@@ -193,30 +104,119 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             try {
                 addMarker(latLngA!!, "A")
                 addMarker(latLngB!!, "B")
-                mMap.addPolyline(polylineABOptions)
-                val dotsRight = Computer.isCrossed(latLngA!!, latLngB!!, northeast!!, southeast!!)
-                val dotsLeft = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, southwest!!)
-                val dotsTop = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, northeast!!)
-                val dotsBottom = Computer.isCrossed(latLngA!!, latLngB!!, southeast!!, southwest!!)
-
-                if (Computer.getLength(dotsLeft, dotsRight) > Computer.getLength(dotsTop, dotsBottom)) {
-                    val polylineAc = mMap.addPolyline(
-                        PolylineOptions()
-                            .add(dotsTop, dotsBottom)
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                } else {
-                    val polylineAx = mMap.addPolyline(
-                        PolylineOptions()
-                            .add(dotsLeft, dotsRight)
-                            .width(3f)
-                            .color(Color.BLUE)
-                            .jointType(JointType.ROUND)
-                    )
-                }
+                mMap.addPolyline(buildOptions(latLngA, latLngB))
+                draw()
             } catch (ex: NullPointerException) { }
+        }
+    }
+
+    //hardcode
+    private fun draw() {
+        val dotsRight = Computer.isCrossed(latLngA!!, latLngB!!, northeast!!, southeast!!)
+        val dotsLeft = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, southwest!!)
+        val dotsTop = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, northeast!!)
+        val dotsBottom = Computer.isCrossed(latLngA!!, latLngB!!, southeast!!, southwest!!)
+
+        val latLngC = Computer.getLatLangC(latLngA, latLngB)
+
+        mMap.addPolyline(buildOptions(latLngA, latLngB))
+
+        val a = Computer.getLength(latLngA, latLngC)
+        val b = Computer.getLength(latLngB, latLngC)
+
+        val cornerA = Computer.getDegreesA(a, b)
+        val cornerB = 180 - 90 - cornerA
+
+        val latLngRightCross = Computer.isCrossed(
+            latLngA!!,
+            latLngB!!,
+            northeast!!,
+            southeast!!
+        )
+        val latLngLeftCross = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, southwest!!)
+        val latLngTopCross = Computer.isCrossed(latLngA!!, latLngB!!, northwest!!, northeast!!)
+        val latLngBottomCross = Computer.isCrossed(
+            latLngA!!,
+            latLngB!!,
+            southeast!!,
+            southwest!!
+        )
+        val step: Double
+
+        if (Computer.getLength(latLngLeftCross, latLngRightCross) > Computer.getLength(
+                latLngTopCross,
+                latLngBottomCross
+            )) {
+            mMap.addPolyline(buildOptions(latLngTopCross, latLngBottomCross))
+            step = DISTANCEM / sin(cornerA)
+            var i = 20
+            var counter1 = latLngTopCross!!.longitude + step
+            var counter2 = latLngBottomCross!!.longitude + step
+
+            while (mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter1, latLngTopCross.latitude)) || mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter2, latLngBottomCross.latitude))) {
+                mMap.addPolyline(
+                    buildOptions(
+                        LatLng(counter1, latLngTopCross.latitude), LatLng(
+                            counter2,
+                            latLngBottomCross.latitude
+                        )
+                    )
+                )
+                counter1 += step
+                counter2 += step
+                i--
+            }
+            counter1 = latLngTopCross.longitude - step
+            counter2 = latLngBottomCross.longitude - step
+            var k = 20
+            while (mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter1, latLngTopCross.latitude)) || mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter2, latLngBottomCross.latitude))) {
+                mMap.addPolyline(
+                    buildOptions(
+                        LatLng(counter1, latLngTopCross.latitude), LatLng(
+                            counter2,
+                            latLngBottomCross.latitude
+                        )
+                    )
+                )
+                counter1 -= step
+                counter2 -= step
+                k--
+            }
+        } else {
+            mMap.addPolyline(buildOptions(latLngLeftCross, latLngRightCross))
+            step = DISTANCEM / sin(cornerB)
+            var i = 20
+            var counter1 = latLngLeftCross!!.latitude + step
+            var counter2 = latLngRightCross!!.latitude + step
+            while (mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter1, latLngLeftCross.longitude)) || mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter2, latLngRightCross.longitude))) {
+                mMap.addPolyline(
+                    buildOptions(
+                        LatLng(counter1, latLngLeftCross.longitude), LatLng(
+                            counter2,
+                            latLngRightCross.longitude
+                        )
+                    )
+                )
+                counter1 += step
+                counter2 += step
+                i--
+            }
+            counter1 = latLngLeftCross.latitude - step
+            counter2 = latLngRightCross.latitude - step
+            var k = 20
+            while (mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter1, latLngLeftCross.longitude)) || mMap.projection.visibleRegion.latLngBounds.contains(LatLng(counter2, latLngRightCross.longitude))) {
+                mMap.addPolyline(
+                    buildOptions(
+                        LatLng(counter1, latLngLeftCross.longitude), LatLng(
+                            counter2,
+                            latLngRightCross.longitude
+                        )
+                    )
+                )
+                counter1 -= step
+                counter2 -= step
+                k--
+            }
         }
     }
 
@@ -224,4 +224,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         options = MarkerOptions().position(latLng).title(title)
         mMap.addMarker(options)
     }
+
+    private fun buildOptions(a: LatLng?, b: LatLng?) = PolylineOptions()
+        .width(3f)
+        .color(Color.BLUE)
+        .jointType(JointType.ROUND)
+        .add(a, b)
+
 }
